9443be49106d02c7d6b9949e4d0b1b72eedfea8a
HyeYeonCha <gpdus335@naver.com>
2019-11-18 17:59:27 +0900

    클리커 게임 초기셋팅 및 버튼 누르면 애니메이션과 함께 점수 증가

---
+++ unity_game_/Assets/Scripts/GM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    [SerializeField]
+    private Text coinText;
+    private float coinCount;
+
+    [SerializeField]
+    private GameObject coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinCount += Time.deltaTime;
+        coinText.text = coinCount.ToString("N0");
+    }
+
+    public void OnClickCoin()
+    {
+        coinCount++;
+    }
+
+
+}
